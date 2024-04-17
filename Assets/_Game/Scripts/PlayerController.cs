4dcd6d04d2622d965777d600d21b0db4abc1636f
TDTer <80698862+TDTer@users.noreply.github.com>
2024-04-17 17:42:20 +0700

    add AddBrick and RemoveBrick

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -23,6 +24,11 @@ public class PlayerController : MonoBehaviour
     public Direction direction;
     public float speed = 10.0f;
     public bool isMoving = false;
+    public GameObject brickPrefab;
+
+    private int countBrick;
+    private float brickHeight;
+    private GameObject[] listOfBricks;
 
 
 
@@ -30,6 +36,9 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         nextPos = transform.position;
+        countBrick = 0;
+        brickHeight = brickPrefab.GetComponent<MeshRenderer>().bounds.size.y;
+        listOfBricks = new GameObject[100];
     }
 
     // Update is called once per frame
@@ -122,7 +131,52 @@ public class PlayerController : MonoBehaviour
             }
             else break;
         }
-        Debug.Log(nextPos);
+        // Debug.Log(nextPos);
         return nextPos;
+    }
+
+    void OnTriggerEnter(Collider collisionInfo)
+    {
+
+        if (collisionInfo.gameObject.CompareTag("Brick"))
+        {
+            if (collisionInfo.gameObject.GetComponent<Renderer>().enabled)
+            {
+                collisionInfo.gameObject.GetComponent<Renderer>().enabled = false;
+                AddBrick(collisionInfo.gameObject);
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider collisionInfo)
+    {
+        if (collisionInfo.gameObject.CompareTag("UnBrick"))
+        {
+            RemoveBrick();
+            Instantiate(brickPrefab, collisionInfo.transform.position, Quaternion.Euler(-90, 0, -180));
+        }
+        Debug.Log(countBrick);
+    }
+
+    void AddBrick(GameObject newBrick)
+    {
+        GameObject myObject = Instantiate(brickPrefab, transform.position, Quaternion.Euler(-90, 0, -180));
+        myObject.transform.parent = transform;
+        myObject.tag = "Untagged";
+        myObject.GetComponent<BoxCollider>().enabled = false;
+        myObject.transform.position = new Vector3(myObject.transform.position.x, myObject.transform.position.y + brickHeight * countBrick - 0.35f, myObject.transform.position.z);
+        listOfBricks[countBrick] = myObject;
+
+        countBrick++;
+        GameObject child = transform.Find("jiao").gameObject;
+        if (countBrick > 1) child.transform.position = new Vector3(child.transform.position.x, child.transform.position.y + brickHeight, child.transform.position.z);
+    }
+
+    void RemoveBrick()
+    {
+        countBrick--;
+        Destroy(listOfBricks[countBrick]);
+        GameObject child = transform.Find("jiao").gameObject;
+        if (countBrick > 1) child.transform.position = new Vector3(child.transform.position.x, child.transform.position.y - brickHeight, child.transform.position.z);
     }
 }
